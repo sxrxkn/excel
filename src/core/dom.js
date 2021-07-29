4fc9889d0b415792a804b07bdf8d507a11e85d5c
sxrxkn <sxrxkn@mail.ru>
2021-07-29 17:09:28 +0300

    table logic is done

--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,17 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
@@ -46,8 +57,37 @@ class Dom {
     return this.$el.getBoundingClientRect()
   }
 
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   findAll(selector) {
     return this.$el.querySelectorAll(selector)
+  }
+
+  id(parse) {
+    if (parse) {
+      const strId = this.id().split(':')
+      const newId = {
+        row: +strId[0],
+        col: +strId[1]
+      }
+      return newId
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  addClass(newClass) {
+    return this.$el.classList.add(newClass)
+  }
+
+  removeClass(newClass) {
+    return this.$el.classList.remove(newClass)
   }
 
   closest(selector) {
--- src/core/utils.js
+++ src/core/utils.js
@@ -5,3 +5,12 @@ export function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
 }
+
+export function range(start, end) {
+  if (end < start) {
+    [end, start] = [start, end]
+  }
+  return new Array(end - start + 1)
+      .fill('')
+      .map((_, index) => start + index)
+}
